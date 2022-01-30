c2269ea6d2cf02df7e90cd50ee8c60621e62c310
Gilberto Walace <gilbertowm>
2022-01-30 10:39:10 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/io/github/gilbertowm/cursomc/resources/CategoriaResource.java
+++ src/main/java/io/github/gilbertowm/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package io.github.gilbertowm.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.gilbertowm.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -13,9 +18,23 @@ public class CategoriaResource {
 		return "REST está funcionando!";
 	}*/
 	
+	
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
+	
+	
 	
 }
