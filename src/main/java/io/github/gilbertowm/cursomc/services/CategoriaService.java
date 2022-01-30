1d2d5ba3b3a2b0fff05db7f206df6e77afb9e3ec
Gilberto Walace <gilbertowm>
2022-01-30 18:36:25 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/io/github/gilbertowm/cursomc/services/CategoriaService.java
+++ src/main/java/io/github/gilbertowm/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import io.github.gilbertowm.cursomc.domain.Categoria;
 import io.github.gilbertowm.cursomc.repositories.CategoriaRepository;
+import io.github.gilbertowm.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,16 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	/*public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
+	}*/
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	
 }
