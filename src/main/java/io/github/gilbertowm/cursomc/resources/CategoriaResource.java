aa4a136f6f931c325bf5130fc86998af86569019
Gilberto Walace <gilbertowm>
2022-01-30 10:13:22 -0300

    REST testado

---
+++ src/main/java/io/github/gilbertowm/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,21 @@
+package io.github.gilbertowm.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	/*@GetMapping
+	public String listar() {
+		return "REST está funcionando!";
+	}*/
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+	
+}
